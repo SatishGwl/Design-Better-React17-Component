dbe9b08be2ddf169204945cf51e8e88b359f207d
apple <apple@apples-MacBook-Air.local>
2023-05-10 17:12:57 +0530

    create nested component and pass data

--- pages/index.js
+++ pages/index.js
@@ -1,40 +1,72 @@
 import {data} from '../SpeakerData';
-console.log('+++data++', data)
+console.log('+++data++', data.sessions)
+
+function Session({title, room}){
+    console.log('+++props+++', title);
+    return(
+        <span className='session w-100'>
+             {title} <strong>Room: {room}</strong>
+        </span>
+    )
+}
+
+function Sessions({sessions}){
+    console.log('+++sessions+++', data.sessions)
+    return(
+        <div className='sessionBox card h-250'>
+            <Session title={sessions[0].title}
+            room={sessions[0].room.name}/>
+        </div>
+    )
+}
+
+const SpeakerImage = ({id, first, last}) =>{
+    return(
+        <div className='speaker-img d-flex flex-row justify-content-center align-item-center h-300'>
+        <img className='contain-fit'
+            src={`/images/speaker-${id}.jpg`}
+            width='300'
+            alt={`{ ${first} ${last} }`}
+        /> 
+    </div>
+    )
+}
+
+const SpeakerDemographics = ({first, last, bio, company, twitterHandler, favourite}) =>{
+    return(
+        <div className='speaker-info'>
+            <div className='d-flex justify-content-between mb-3'>
+                <h3 className='text-turncate w-200'>
+                    {first} {last}
+                </h3>
+            </div>
+            <div>
+                <p>{bio}{company}{twitterHandler}{favourite}</p>
+            </div>
+        </div>
+    )
+}
+
+const Speaker =({speaker})=>{
+    const {id, first, last, sessions} = speaker;
+    return(
+        <div key={id} className='col-xs-12 col-sm-12 col-md-6 col-lg-4 col-sm-12 col-xs-12'>
+        <div className='card card-heigth p-4 mt-4'>
+            <SpeakerImage id={id} first={first} last={last}/>
+            <SpeakerDemographics {...speaker}/>
+            <Sessions sessions={sessions}/>
+        </div>
+    </div>
+    )
+}
 
 const IndexPage = ()=> {
     return(
         <div className='container speaker-list'>
             <div className='row'>
                 {data.map(function(speaker){
-                    const {id, bio, first, last, favourite, twitterHandler, company, sessions } = speaker;
-                    console.log('+++Speaker++', typeof(speaker))
                     return(
-                        <div key={id} className='col-xs-12 col-sm-12 col-md-6 col-lg-4 col-sm-12 col-xs-12'>
-                        <div className='card card-heigth p-4 mt-4'>
-                            <div className='speaker-img d-flex flex-row justify-content-center align-item-center h-300'>
-                                <img className='contain-fit'
-                                    src={`/images/speaker-${id}.jpg`}
-                                    width='300'
-                                    alt={`{ ${first} ${last} }`}
-                                /> 
-                            </div>
-                            <div className='speaker-info'>
-                                <div className='d-flex justify-content-between mb-3'>
-                                    <h3 className='text-turncate w-200'>
-                                        {first} {last}
-                                    </h3>
-                                </div>
-                                <div>
-                                    <p>{bio}{company}{twitterHandler}{favourite}</p>
-                                </div>
-                            </div>
-                            <div className='sessionBox card h-250'>
-                                <span className='session w-100'>
-                                    {sessions[0].title}
-                                </span>
-                            </div>
-                        </div>
-                    </div>
+                       <Speaker key={speaker.id} speaker={speaker}/>
                     )
                 })}
             </div>
