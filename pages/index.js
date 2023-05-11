70c34c5d977425c8e8266d7f7e6570e983c8653c
apple <apple@apples-MacBook-Air.local>
2023-05-11 12:54:30 +0530

    add header and toolbar component and set state

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,6 @@
-import {data} from '../SpeakerData';
-import Speaker from '../public/src/Speaker';
+import Speakers from "../src/components/Speakers";
+const IndexPage = () => {
+  return <Speakers />;
+};
 
-const IndexPage = ()=> {
-    return(
-        <div className='container speaker-list'>
-            <div className='row'>
-                {data.map(function(speaker){
-                    return(
-                       <Speaker key={speaker.id} speaker={speaker}/>
-                    )
-                })}
-            </div>
-        </div>
-    )
-}
 export default IndexPage;
