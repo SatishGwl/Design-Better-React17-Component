ef1ecc989b7aeca2aff6ebc27ea531dad441e1c0
SatishGwl <satish.srcem@gmail.com>
2023-05-15 01:45:22 +0530

    implemented progress when updating records

--- src/components/SpeakersList.js
+++ src/components/SpeakersList.js
@@ -39,11 +39,11 @@ function SpeakersList({ showSessions }) {
                 key={speaker.id}
                 speaker={speaker}
                 showSessions={showSessions}
-                onFavoriteToggle={() => {
+                onFavoriteToggle={(doneCallback) => {
                   updateRecord({
                     ...speaker,
                     favorite: !speaker.favorite,
-                  });
+                  }, doneCallback);
                 }}
               />
             );
